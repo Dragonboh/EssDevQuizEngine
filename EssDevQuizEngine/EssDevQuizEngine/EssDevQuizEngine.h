47b0074acd5d1501939c21c71fff896bf1d62955
Bogdan Pankevych <>
2024-06-25 18:29:23 +0200

    initial commit

---
+++ EssDevQuizEngine/EssDevQuizEngine/EssDevQuizEngine.h
@@ -0,0 +1,18 @@
+//
+//  EssDevQuizEngine.h
+//  EssDevQuizEngine
+//
+//  Created by admin on 25.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssDevQuizEngine.
+FOUNDATION_EXPORT double EssDevQuizEngineVersionNumber;
+
+//! Project version string for EssDevQuizEngine.
+FOUNDATION_EXPORT const unsigned char EssDevQuizEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssDevQuizEngine/PublicHeader.h>
+
+
