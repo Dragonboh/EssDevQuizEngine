e2767d412f76ecb0c18df06fa8417cb678bc29ad
Bogdan Pankevych <>
2024-07-03 13:03:15 +0200

    make Flow class and Router protocol generic to accept different types for Questions and Answers

--- EssDevQuizEngine/EssDevQuizEngine/Flow.swift
+++ EssDevQuizEngine/EssDevQuizEngine/Flow.swift
@@ -8,41 +8,45 @@
 import Foundation
 
 protocol Router {
-    typealias AnswerCallback = (String) -> Void
-    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
-    func routeTo(results: [String : String])
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+//    typealias AnswerCallback = (Answer) -> Void
+    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func routeTo(results: [Question : Answer])
 }
 
-final class Flow {
-    private let router: Router
-    private let questions: [String]
-    private var results: [String : String] = [:]
+final class Flow<Question: Hashable, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
     
-    init(questions: [String], router: Router) {
+    private let router: R
+    private let questions: [Question]
+    private var results: [Question : Answer] = [:]
+    
+    init(questions: [Question], router: R) {
         self.questions = questions
         self.router = router
     }
     
     func start() {
         if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion, answerCallback: nexxtCallback(question: firstQuestion))
+            router.routeTo(question: firstQuestion, answerCallback: nextCallback(question: firstQuestion))
         } else {
             router.routeTo(results: results)
         }
     }
     
-    private func nexxtCallback(question: String) -> Router.AnswerCallback {
+    private func nextCallback(question: Question) -> (Answer) -> Void {
         return { [weak self] answer in
             self?.route(question: question, answer: answer)
         }
     }
-    private func route(question: String, answer: String) {
+    private func route(question: Question, answer: Answer) {
         if let currentQuestionIndex = questions.firstIndex(of: question) {
             results[question] = answer
             let nextQuestionindex = currentQuestionIndex + 1
             if nextQuestionindex < questions.count {
                 let nextQuestion = questions[nextQuestionindex]
-                router.routeTo(question: nextQuestion, answerCallback: nexxtCallback(question:nextQuestion))
+                router.routeTo(question: nextQuestion, answerCallback: nextCallback(question:nextQuestion))
             } else {
                 router.routeTo(results: results)
             }
